183d083fa097136c0cd5f160702015885521d62d
SIZWE <Sizwemtshali100@gmail.com>
2023-03-27 19:26:34 +0200

    updated

--- SouthSide/settings.py
+++ SouthSide/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,7 +133,6 @@ STATIC_URL = 'static/'
 STATIC_FILES = [ 
     BASE_DIR / '/static/'
 ]
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
